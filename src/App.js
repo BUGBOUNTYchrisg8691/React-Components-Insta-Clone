a43fa53b5e1c2821819883e1c5c9a1a451c8315b
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-04 02:48:32 -0400

    stuck on add comment functionality

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-  const [posts, setPosts] = useState(dummyData);
+  const [posts, setPosts] = useState(dummyData)
+
   // const [search, setSearch] = useState('');
 
   // const searchPost = (value, postUser) => {
@@ -39,7 +40,7 @@ const App = () => {
      */
     setPosts(posts.map(post => {
       if(post.id === postId) {
-        return {...post, likes: post.likes + 1}
+        return { ...post, likes: post.likes + 1 }
       } 
       return post
     }))
@@ -50,7 +51,7 @@ const App = () => {
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
       <SearchBar />
-      <Posts likePost={likePost} posts={posts} />
+      <Posts likePost={ likePost } posts={ posts } />
     </div>
   );
 };
